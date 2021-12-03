beb2a812287813838bd09470b2e337f0e1d6cb7b
LiamGallagher737 <ljpgallagher@xtra.co.nz>
2021-12-04 11:17:01 +1300

    20 minutes into 3 hours

---
+++ Inconvenient Superpowers/Assets/Scripts/Init.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine; using System.Collections;
+public class Init : MonoBehaviour
+{
+    private void Start() {
+        GameManager.Init();
+    }
+}
---
+++ Inconvenient Superpowers/Assets/Scripts/MapManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+public class MapManager : MonoBehaviour {
+
+    [SerializeField] private GameObject platformPrefab;
+    [SerializeField] private Vector2 mapSize;
+    [SerializeField] private int platformDensity = 5;
+
+    public void Start() {
+        for (var i = 0; i < platformDensity; i++) {
+            AddNewPlatform();
+        }
+    }
+
+    private void AddNewPlatform() {
+        var pos = new Vector3(Random.Range(0, mapSize.x)- (mapSize.x / 2), Random.Range(0, mapSize.y)- (mapSize.y / 2), 0);
+        Instantiate(platformPrefab, pos, Quaternion.identity);
+    }
+}
